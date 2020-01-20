82bb793cc2add6b5953156153d17ad0e4505173d
BillaSundar <sundaravinash79@gmail.com>
2020-01-20 18:58:46 +0530

    changed

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,29 @@
 import express from 'express';
+
 import { Tablecreation } from './tablegenerator';
+import { School } from './school';
+import { teachersData } from './sampleData';
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  var output =new Tablecreation;
+  var sample = new School("Sample", 6, 8);
+
+  sample.importTeachers(teachersData);
+
+  sample.addClassRoom("I");
+  sample.addClassRoom("II");
+  sample.addClassRoom("III");
+  sample.addClassRoom("IV");
+  sample.addClassRoom("V");
+  sample.addClassRoom("VI");
+  sample.addClassRoom("VII");
+  sample.addClassRoom("VIII");
+
+  sample.start();
+  
+  var output = new Tablecreation(sample.workingDays, sample.classRooms);
   res.send(output.generator());
 })
 
